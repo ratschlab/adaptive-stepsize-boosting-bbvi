3d4f86745e19ee31dc208a00c3c98479838d5d75
Saurav Shekhar <shekhars@student.ethz.ch>
2019-05-08 16:15:17 +0200

    matrix factorization initial commit

--- boosting_bbvi/scripts/matrix_factorization.py
+++ boosting_bbvi/scripts/matrix_factorization.py
@@ -1,14 +1,13 @@
 """Probabilistic matrix factorization using variational inference.
 
 Visualizes the actual and the estimated rating matrices as heatmaps.
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
-import os
-import matplotlib
-matplotlib.use('Agg')
+Follows the Edward example:-
+https://github.com/blei-lab/edward/blob/master/examples/probabilistic_matrix_factorization.py
+"""
+import os, sys
+#import matplotlib
+#matplotlib.use('Agg')
 
 import edward as ed
 import matplotlib.pyplot as plt
@@ -16,137 +15,196 @@ import numpy as np
 import tensorflow as tf
 import scipy.io as sio
 
-from edward.models import Normal, MultivariateNormalDiag, Mixture, Categorical, ParamMixture
+from edward.models import (Normal, MultivariateNormalDiag, Mixture,
+                           Categorical, ParamMixture)
 
-from boosting_bbvi.core.utils import block_diagonal
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 import boosting_bbvi.core.relbo as relbo
-from boosting_bbvi.core.mvn import mvn
+from boosting_bbvi.core.utils import block_diagonal, eprint, debug, append_to_file
+import boosting_bbvi.core.utils as coreutils
+logger = coreutils.get_logger()
 
 flags = tf.app.flags
 FLAGS = tf.flags.FLAGS
+flags.DEFINE_string('outdir', '/tmp', 'directory to log all the results etc.')
+flags.DEFINE_integer("D", 3, "Number of latent factors.")
 flags.DEFINE_integer("N", 50, "Number of users.")
 flags.DEFINE_integer("M", 60, "Number of movies.")
-flags.DEFINE_integer("D", 3, "Number of latent factors.")
-flags.DEFINE_integer('n_fw_iter', 1000, '')
+flags.DEFINE_integer('n_fw_iter', 10, '')
+flags.DEFINE_integer('LMO_iter', 1000, '')
 flags.DEFINE_integer('seed', 0, 'The random seed to use for everything.')
-flags.DEFINE_string('outdir', '/tmp', '')
-flags.DEFINE_float('mask_ratio', 0.5, '')
+flags.DEFINE_float('mask_ratio', 0.5, 'Test train indicator matrix mask ratio')
+tf.flags.DEFINE_enum('exp', 'cbcl', ['synthetic', 'cbcl'], 'Dataset name')
+tf.flags.DEFINE_enum(
+    "base_dist", 'mvn',
+    ['normal', 'laplace', 'mvnormal', 'mvlaplace', 'mvn', 'mvl'],
+    'base distribution for variational approximation')
+flags.DEFINE_enum('fw_variant', 'fixed', ['fixed'],
+                  '[fixed (default)] The Frank-Wolfe variant to use.')
+tf.flags.DEFINE_string('datapath', 'data/chem', 'path containing data')
 
 ed.set_seed(FLAGS.seed)
 np.random.seed(FLAGS.seed)
 
+
 def build_toy_dataset(U, V, N, M, noise_std=0.1):
-  R = np.dot(np.transpose(U), V) + np.random.normal(0, noise_std, size=(N, M))
-  return R
+    R = np.dot(np.transpose(U), V) + np.random.normal(
+        0, noise_std, size=(N, M))
+    return R
+
 
 def get_indicators(N, M, prob_std=0.5):
-  ind = np.random.binomial(1, prob_std, (N, M))
-  return ind
+    ind = np.random.binomial(1, prob_std, (N, M))
+    return ind
 
-def flatten(a):
-    n,m = a.shape
-    return np.reshape(a, n*m)
 
-def build_mixture(weights, qus):
-  cat = Categorical(probs=tf.convert_to_tensor(weights))
-  comps = [mvn(loc=tf.convert_to_tensor(c['loc']),scale=tf.convert_to_tensor(c['scale'])) for c in qus]
+def get_data():
+    if FLAGS.exp == 'cbcl':
+        tr = sio.loadmat(os.path.join(FLAGS.datapath, 'cbcl.mat'))['V']
+        N,M = tr.shape
+        I_train = get_indicators(N, M, FLAGS.mask_ratio)
+        I_test = 1 - I_train
+        return N, M, FLAGS.D, tr, I_train, I_test
+    elif FLAGS.exp == 'synthetic':
+        N, M, D = FLAGS.N, FLAGS.M, FLAGS.D
+        # true latent factors
+        U_true = np.random.randn(D, N)
+        V_true = np.random.randn(D, M)
+        R_true = build_toy_dataset(U_true, V_true, N, M)
+        I_train = get_indicators(N, M, FLAGS.mask_ratio)
+        I_test = 1 - I_train
+        return N, M, D, R_true, I_train, I_test
+    pass
 
-  mix = Mixture(cat=cat, components=comps)
-  return mix
-
-def get_fw_iterates(iter, weights, target, qUVt_components):
-  ret = {}
-  if iter > 0:
-    ret = {target: build_mixture(weights, qUVt_components)}
-  return ret
-
-def update_iterate(components, s):
-  loc = s.loc.eval()
-  scale = s.scale.eval()
-  components.append({'loc': loc, 'scale': scale})
-  return components
 
 def main(_):
-  # true latent factors
-  U_true = np.random.randn(FLAGS.D, FLAGS.N)
-  V_true = np.random.randn(FLAGS.D, FLAGS.M)
+    # setting up output directory
+    outdir = os.path.expanduser(FLAGS.outdir)
+    os.makedirs(outdir, exist_ok=True)
 
-  ## DATA
-  #R_true = build_toy_dataset(U_true, V_true, FLAGS.N, FLAGS.M)
-  #I_train = get_indicators(FLAGS.N, FLAGS.M)
-  #I_test = 1 - I_train
-  #N = FLAGS.N
-  #M = FLAGS.M
+    N, M, D, R_true, I_train, I_test = get_data()
 
-  #tr = sio.loadmat(os.path.expanduser("~/data/bbbvi/trainData1.mat"))['X']
-  #te = sio.loadmat(os.path.expanduser("~/data/bbbvi/testData1.mat"))['X']
-  #tr = tr[:,:100]
-  #te = te[:,:100]
-  #I_train = tr != 0
-  #I_test = te != 0
-  #R_true = (tr + te).astype(np.float32)
-  #N,M = R_true.shape
+    # Solution components
+    weights, qUVt_components = [], []
 
-  tr = sio.loadmat(os.path.expanduser("~/data/bbbvi/cbcl.mat"))['V']
-  te = sio.loadmat(os.path.expanduser("~/data/bbbvi/cbcl.mat"))['V']
-  #I_train = np.ones(tr.shape)
-  #I_test = np.ones(tr.shape)
-  R_true = tr
-  N,M = tr.shape
-  D = FLAGS.D
-  I_train = get_indicators(N, M, FLAGS.mask_ratio)
-  I_test = 1 - I_train
+    # Metrics to log
+    # TODO replace it with file logging
+    mses, test_mses, test_lls = [], [], []
 
-  it_best = 0
-  weights, qUVt_components, mses = [], [], []
-  test_mses, test_lls = [], []
-  for iter in range(FLAGS.n_fw_iter):
-    print("iter", iter)
-    g = tf.Graph()
-    with g.as_default():
-        tf.set_random_seed(FLAGS.seed)
-        sess = tf.InteractiveSession()
-        with sess.as_default():
-          # MODEL
-          I = tf.placeholder(tf.float32, [N, M])
+    for t in range(FLAGS.n_fw_iter):
+        g = tf.Graph()
+        with g.as_default():
+            tf.set_random_seed(FLAGS.seed)
+            sess = tf.InteractiveSession()
+            with sess.as_default():
+                # MODEL
+                I = tf.placeholder(tf.float32, [N, M])
 
-          scale_uv = tf.concat([tf.ones([FLAGS.D, N]), tf.ones([FLAGS.D, M])], axis=1)
-          mean_uv = tf.concat([tf.zeros([FLAGS.D, N]), tf.zeros([FLAGS.D, M])], axis=1)
+                scale_uv = tf.concat(
+                    [tf.ones([D, N]),
+                     tf.ones([D, M])], axis=1)
+                mean_uv = tf.concat(
+                    [tf.zeros([D, N]),
+                     tf.zeros([D, M])], axis=1)
 
-          UV = Normal(loc=mean_uv, scale=scale_uv)
-          R = Normal(loc=tf.matmul(tf.transpose(UV[:, :N]), UV[:, N:]) * I, scale=tf.ones([N, M]))
-          mean_quv = tf.concat([tf.get_variable("qU/loc", [FLAGS.D, N]), tf.get_variable("qV/loc", [FLAGS.D, M])],
-                               axis=1)
-          scale_quv = tf.concat([tf.nn.softplus(tf.get_variable("qU/scale", [FLAGS.D, N])),
-                                 tf.nn.softplus(tf.get_variable("qV/scale", [FLAGS.D, M]))], axis=1)
+                UV = Normal(loc=mean_uv, scale=scale_uv)
+                debug('UV', UV.event_shape, UV.batch_shape)
+                R = Normal(
+                    loc=tf.matmul(tf.transpose(UV[:, :N]), UV[:, N:]) * I,
+                    scale=tf.ones([N, M]))
 
-          qUV = Normal(loc=mean_quv, scale=scale_quv)
+                # TODO build previous components and add here
+                if t == 0:
+                    fw_iterates = {}
+                else:
+                    # Current solution
+                    prev_components = [
+                        coreutils.base_loc_scale(
+                            'mvnormal',
+                            c['loc'],
+                            c['scale'],
+                            multivariate=False) for c in qUVt_components
+                    ]
+                    qUV_prev = coreutils.get_mixture(weights, prev_components)
+                    fw_iterates = {UV: qUV_prev}
 
-          inference = relbo.KLqp({UV: qUV}, data={R: R_true, I: I_train},
-                        fw_iterates=get_fw_iterates(iter, weights, UV, qUVt_components),
-                        fw_iter=iter)
-          inference.run(n_iter=100)
+                # LMO (via relbo INFERENCE)
+                mean_suv = tf.concat([
+                    tf.get_variable("qU/loc", [D, N]),
+                    tf.get_variable("qV/loc", [D, M])
+                ],
+                                     axis=1)
+                scale_suv = tf.concat([
+                    tf.nn.softplus(tf.get_variable("qU/scale", [D, N])),
+                    tf.nn.softplus(tf.get_variable("qV/scale", [D, M]))
+                ],
+                                      axis=1)
 
-          gamma = 2. / (iter + 2.)
-          weights = [(1. - gamma) * w for w in weights]
-          weights.append(gamma)
+                sUV = Normal(loc=mean_suv, scale=scale_suv)
+                debug('sUV', sUV.event_shape, sUV.batch_shape)
+                debug('t is ', t)
 
-          qUVt_components = update_iterate(qUVt_components, qUV)
+                inference = relbo.KLqp({UV: sUV}, data={R: R_true, I: I_train},
+                                       fw_iterates=fw_iterates, fw_iter=t)
+                inference.run(n_iter=FLAGS.LMO_iter)
 
-          qUV_new = build_mixture(weights, qUVt_components)
-          qR = Normal(loc=tf.matmul(tf.transpose(qUV_new[:, :N]), qUV_new[:, N:]), scale=tf.ones([N, M]))
+                loc_s = sUV.mean().eval()
+                scale_s = sUV.stddev().eval()
 
-          # CRITICISM
-          test_mse = ed.evaluate('mean_squared_error', data={qR: R_true, I: I_test.astype(bool)})
-          test_mses.append(test_mse)
-          print('test mse', test_mse)
+                # TODO move this to fixed step
+                gamma = 2. / (t + 2.)
+                weights = [(1. - gamma) * w for w in weights]
+                weights.append(gamma)
+                qUVt_components.append({'loc': loc_s, 'scale': scale_s})
 
-          test_ll = ed.evaluate('log_lik', data={qR: R_true.astype('float32'), I: I_test.astype(bool)})
-          test_lls.append(test_ll)
-          print('test_ll', test_ll)
+                if t == 0:
+                    gamma = 1.
+                    new_components = [sUV]
+                else:
+                    new_components = [
+                        coreutils.base_loc_scale(
+                            'normal',
+                            c['loc'],
+                            c['scale'],
+                            multivariate=False) for c in qUVt_components
+                    ]
 
-          np.savetxt(os.path.join(FLAGS.outdir, 'test_mse.csv'), test_mses, delimiter=',')
-          np.savetxt(os.path.join(FLAGS.outdir, 'test_ll.csv'), test_lls, delimiter=',')
+                qUV_new = coreutils.get_mixture(weights, new_components)
+
+                qR = Normal(
+                    loc=tf.matmul(
+                        tf.transpose(qUV_new[:, :N]), qUV_new[:, N:]),
+                    scale=tf.ones([N, M]))
+
+                # CRITICISM
+                test_mse = ed.evaluate(
+                    'mean_squared_error',
+                    data={
+                        qR: R_true,
+                        I: I_test.astype(bool)
+                    })
+                test_mses.append(test_mse)
+                print('test mse', test_mse)
+
+                test_ll = ed.evaluate(
+                    'log_lik',
+                    data={
+                        qR: R_true.astype('float32'),
+                        I: I_test.astype(bool)
+                    })
+                test_lls.append(test_ll)
+                print('test_ll', test_ll)
+
+                #np.savetxt(
+                #    os.path.join(FLAGS.outdir, 'test_mse.csv'),
+                #    test_mses,
+                #    delimiter=',')
+                #np.savetxt(
+                #    os.path.join(FLAGS.outdir, 'test_ll.csv'),
+                #    test_lls,
+                #    delimiter=',')
+                sess.close()
+
 
 if __name__ == "__main__":
-  tf.app.run(main)
+    tf.app.run(main)
